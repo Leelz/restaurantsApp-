a2d728dc7f37eb8b476af1c4bfc872f6b60ef940
Leelz <adam.leel93@gmail.com>
2018-08-31 22:12:45 +0100

    Update restaurantApi.js

--- restaurantApi.js
+++ restaurantApi.js
@@ -1,26 +1,26 @@
 [
 {
-	id: "1",
-	name: "Absolute Thai",
-	latlng: [55.941685,-3.2026829000000134],
-	address: "22 Valleyfield Street Edinburgh EH3 9LR",
-	phone_number: "0131 228 8022",
-	evening_deal: "12.50 for 2 courses",
-	availability: "Daily 5.30–7pm",
-	type: "Thai",
-	contact: "contact@absolutethai.co.uk",
-	website: "www.absolutethai.co.uk"
+"id": "1",
+"Name": "Absolute Thai",
+"LatLng": "[55.941685,-3.2026829000000134]",
+"Address": "22 Valleyfield Street Edinburgh EH3 9LR",
+"PhoneNumber": "0131 228 8022",
+"EveningDeal": "12.50 for 2 courses",
+"Availability": "Daily 5.30–7pm",
+"Type": "Thai",
+"Contact": "contact@absolutethai.co.uk",
+"Website": "www.absolutethai.co.uk"
 },
 {
-	id: "2",
-	name: "Amarone",
-	latlng: [55.9537467,-3.1944038999999975],
-	address: "13 St Andrew Square Edinburgh EH2 2AF",
-	phone_number: "0131 523 1171",
-	evening_deal: "£15.95 (£18.95 three courses)",
-	availability: "Sun–Fri noon–6.30pm; Sat noon–3pm.",
-	type: "Italian Seafood",
-	contact: "info@amaronerestaurant.co.uk",
-	website: "www.amaronerestaurant.co.uk"
+"id": "2",
+"Name": "Amarone",
+"LatLng": "[55.9537467,-3.1944038999999975]",
+"Address": "13 St Andrew Square Edinburgh EH2 2AF",
+"PhoneNumber": "0131 523 1171",
+"EveningDeal": "£15.95 (£18.95 three courses)",
+"Availability": "Sun–Fri noon–6.30pm; Sat noon–3pm.",
+"Type": "Italian Seafood",
+"Contact": "info@amaronerestaurant.co.uk",
+"Website": "www.amaronerestaurant.co.uk"
 }
-]
+	]
